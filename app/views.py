4c1bad1d56513902032e7055182d4cd22c879367
rayv28 <revanth2807@gmail.com>
2023-11-21 13:55:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def fun1(request):
+  return render(request,'rayv.html')
+def fun2(request):
+  return render(request,'revv.html')
+def fun3(request):
+  return render(request,'revanth.html')
